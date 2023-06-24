e5e53d2f6a390361fd6fadd77c2e84ddbdbfd740
Ayush Singh <mr.technical19@gmail.com>
2023-06-24 22:17:15 +0530

    fixes

--- src/StyledComponents/GlobalStyle.jsx
+++ src/StyledComponents/GlobalStyle.jsx
@@ -1,12 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-html{
+html,body{
     background-color: black;
     color: #fff;
     font-family: 'Poppins', sans-serif;
     overflow-x: hidden;
     scroll-behavior: smooth;
+    height: 100%;
+    width: 100%;
 }
 
 *{
